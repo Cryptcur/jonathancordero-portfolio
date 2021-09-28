5e0209db5c6e920cb670707bb46e240a8986f855
Jonathan Cordero <cryptcur00101@protonmail.com>
2021-09-28 18:03:17 -0400

    testing

--- db.js
+++ db.js
@@ -6,20 +6,28 @@ const client = new pg.Client(
 client.connect();
 
 const sync = async () => {
-  const SQL = `
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-    DROP TABLE IF EXISTS users;
-    CREATE TABLE users(
-        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        name VARCHAR(100)
-    );
-    INSERT INTO users(name) values('lucy');
-    INSERT INTO users(name) values('moe');
-    `;
-  await client.query(SQL);
+  try {
+    const SQL = `
+          CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+          DROP TABLE IF EXISTS users;
+          CREATE TABLE users(
+              id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+              name VARCHAR(100)
+          );
+          INSERT INTO users(name) values('lucy');
+          INSERT INTO users(name) values('moe');
+          `;
+    await client.query(SQL);
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 const readUsers = async () => {
-  return (await client.query("SELECT * from users")).rows;
+  try {
+    return (await client.query("SELECT * from users;")).rows;
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
+// require("dotenv").config();
 const express = require("express");
+const app = express();
 const path = require("path");
 const db = require("./db");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
+// app.use(express.json());
 app.use("/dist", express.static(path.join(__dirname, "dist")));
 
 app.get("/", (req, res, next) =>
   res.sendFile(path.join(__dirname, "index.html"))
 );
 
-app.get("/api/users", (req, res, next) => {
-  db.readUsers()
-    .then(users => res.send(users))
-    .catch(next);
+app.get("/api/users", async (req, res, next) => {
+  try {
+    await db
+      .readUsers()
+      .then(users => {
+        console.log("USERS: ", users);
+        res.send(users);
+      })
+      .catch(next);
+  } catch (ex) {
+    console.log(ex);
+  }
 });
 
 db.sync()
